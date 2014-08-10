29ef53991151771943f4411f24ce53a4b99a8555
Robpol86 <robpol86@gmail.com>
2014-08-09 19:48:30 -0700

    Added testpdb to setup.py, partially rewrote.
    Copied from Flask-Statics-Helper.

--- setup.py
+++ setup.py
@@ -10,27 +10,40 @@ import setuptools
 import setuptools.command.sdist
 from setuptools.command.test import test
 
-
-setattr(setuptools.command.sdist, 'READMES',
-        tuple(list(getattr(setuptools.command.sdist, 'READMES', ())) + ['README.md']))
-here = os.path.abspath(os.path.dirname(__file__))
+HERE = os.path.abspath(os.path.dirname(__file__))
+setuptools.command.sdist.READMES = tuple(list(getattr(setuptools.command.sdist, 'READMES', ())) + ['README.md'])
 
 
-# Get the long description and other data from the relevant files
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-with open(os.path.join(here, 'flask_jira.py'), encoding='utf-8') as f:
-    lines = [l.strip() for l in f if l.startswith('__')]
-metadata = ast.literal_eval("{'" + ", '".join([l.replace(' = ', "': ") for l in lines]) + '}')
-__author__, __license__, __version__ = [metadata[k] for k in ('__author__', '__license__', '__version__')]
-if not all((__author__, __license__, __version__)):
-    raise ValueError('Failed to obtain metadata from module.')
+def get_metadata(main_file):
+    """Get metadata about the package/module.
+
+    Positional arguments:
+    main_file -- python file path within `HERE` which has __author__ and the others defined as global variables.
+
+    Returns:
+    Dictionary to be passed into setuptools.setup().
+    """
+    with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+    with open(os.path.join(HERE, main_file), encoding='utf-8') as f:
+        lines = [l.strip() for l in f if l.startswith('__')]
+    metadata = ast.literal_eval("{'" + ", '".join([l.replace(' = ', "': ") for l in lines]) + '}')
+    __author__, __license__, __version__ = [metadata[k] for k in ('__author__', '__license__', '__version__')]
+
+    everything = dict(version=__version__, long_description=long_description, author=__author__, license=__license__)
+    if not all(everything.values()):
+        raise ValueError('Failed to obtain metadata from package/module.')
+
+    return everything
 
 
 class PyTest(test):
+    TEST_ARGS = ['tests']
+
     def finalize_options(self):
         test.finalize_options(self)
-        setattr(self, 'test_args', ['tests'])
+        setattr(self, 'test_args', self.TEST_ARGS)
         setattr(self, 'test_suite', True)
 
     def run_tests(self):
@@ -40,42 +53,33 @@ class PyTest(test):
         sys.exit(err_no)
 
 
+class PyTestPdb(PyTest):
+    TEST_ARGS = ['--pdb', 'tests']
+
+
 class PyTestCov(PyTest):
-    def finalize_options(self):
-        test.finalize_options(self)
-        setattr(self, 'test_args', ['--cov', 'flask_jira', 'tests'])
-        setattr(self, 'test_suite', True)
+    TEST_ARGS = ['--cov', 'flask_jira', 'tests']
 
 
 class PyTestCovWeb(PyTest):
-    def finalize_options(self):
-        test.finalize_options(self)
-        setattr(self, 'test_args', ['--cov-report', 'html', '--cov', 'flask_jira', 'tests'])
-        setattr(self, 'test_suite', True)
+    TEST_ARGS = ['--cov-report', 'html', '--cov', 'flask_jira', 'tests']
 
     def run_tests(self):
         if find_executable('open'):
-            atexit.register(lambda: subprocess.call(['open', os.path.join(here, 'htmlcov', 'index.html')]))
+            atexit.register(lambda: subprocess.call(['open', os.path.join(HERE, 'htmlcov', 'index.html')]))
         PyTest.run_tests(self)
 
 
 # Setup definition.
 setuptools.setup(
     name='Flask-JIRA-Helper',
-    version=__version__,
-
     description='JIRA support for Flask without breaking PyCharm inspections.',
-    long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/Robpol86/Flask-JIRA-Helper',
 
     # Author details
-    author=__author__,
     author_email='robpol86@gmail.com',
-
-    # Choose your license
-    license=__license__,
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -116,5 +120,8 @@ setuptools.setup(
     install_requires=['Flask', 'jira'],
 
     tests_require=['pytest'],
-    cmdclass=dict(test=PyTest, testcov=PyTestCov, testcovweb=PyTestCovWeb),
+    cmdclass=dict(test=PyTest, testpdb=PyTestPdb, testcov=PyTestCov, testcovweb=PyTestCovWeb),
+
+    # Pass the rest from get_metadata().
+    **get_metadata(os.path.join('flask_jira.py'))
 )
